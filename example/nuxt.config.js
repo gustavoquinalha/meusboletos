ba9ea4026564b5aef37565ec49b05c2672a7b257
gustavoquinalha <gustavo_quinalha@hotmail.com>
2019-09-23 21:20:41 -0300

    Deploy

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -1,8 +1,24 @@
-const { resolve } = require('path')
+const { resolve } = require('path');
 
 module.exports = {
   router: {
-    base: '/meusboletos/'
+    base: '/meusboletos/',
+    head: {
+      title: 'Meusboletos',
+      meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Meusboletos'
+      }
+      ]
+    }
   },
   rootDir: resolve(__dirname, '..'),
   buildDir: resolve(__dirname, '.nuxt'),
